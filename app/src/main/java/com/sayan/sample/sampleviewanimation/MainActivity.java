a9cbd6e5c420fb4d1ecd15a4508fdf218aaae975
sayan <anshu.p@encoders.co.in>
2018-07-06 18:46:46 +0530

    basic work done

--- app/src/main/java/com/sayan/sample/sampleviewanimation/MainActivity.java
+++ app/src/main/java/com/sayan/sample/sampleviewanimation/MainActivity.java
@@ -1,26 +1,34 @@
 package com.sayan.sample.sampleviewanimation;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     private RelativeLayout mRoot;
     private float xDelta;
     private float yDelta;
+    private ImageView humanFaceImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRoot = (RelativeLayout) findViewById(R.id.root);
-        TextView text = findViewById(R.id.text);
-        text.setOnTouchListener(this);
+        ImageView shirtImage = findViewById(R.id.shirtImage);
+        humanFaceImage = findViewById(R.id.humanFaceImage);
+        shirtImage.setOnTouchListener(this);
     }
 
     public boolean onTouch(View view, MotionEvent event) {
@@ -46,5 +54,37 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
 //        Log.i("EVENT", "EVENT_X = " + EVENT_X + ", EVENT_Y = " + EVENT_Y);
 //        Log.d("VIEW", "VIEW_X = " + VIEW_X + ", VIEW_Y = " + VIEW_Y);
+    }
+
+    private void onClickCameraButton(){
+        new CameraPicProvider(
+                this,
+                false,false,
+                new CameraPicProvider.GetBitmapListener() {
+
+                    @Override
+                    public void onGetBitmap(Bitmap bitmapImage, String filepath) {
+                        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                        bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                        humanFaceImage.setImageBitmap(bitmapImage);
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.cameraButton) {
+            onClickCameraButton();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
